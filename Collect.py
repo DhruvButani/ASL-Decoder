0123bd01e1ba61eb606ca6987f925b7857ec19b5
dhruvbutani <dhruv.butani24@gmail.com>
2024-09-10 23:36:59 -0500

    1

--- Collect.py
+++ Collect.py
@@ -2,34 +2,60 @@ import cv2 as cv
 import os
 
 
-
-if 
-
+# Per letter take 100 pictures
+# Take 100 pictures per everytime s is pressed
+# 
+#
+#
 cap = cv.VideoCapture(0) #0 is default webcam
 
-if cap.isOpened is True:
-    print('Webcam could not be opened')
-    exit()
+DIR = './data'
 
-while(True):
-    valid, image = cap.read()
+datasize = 200
+signals = 36
+images = 0
+
+
+for uploaded in range(36):
+
+    b = False
+    while(True):
+        valid, image = cap.read()
+        
+        if not valid:
+            print('Webcam could not be opened')
+            break
+        cv.imshow('Live Webcam',image)
+
+        if cv.waitKey(1) == ord('q'):
+            print('Exiting')
+            b = True
+            break
+
+        if cv.waitKey(1) == ord('s'):
+            print('Capturing')
+            break
+
+    if not b:
+        break
+
+    uploaded = 0
+    while(uploaded<datasize):
+        valid, image = cap.read()
+        if not valid:
+            print('Webcam could not be opened')
+            break
+        cv.imshow('Live Webcam',image)
+
+        cv.waitKey(25)
+        cv.imwrite(folder=, img=image, )
+
+
+
+
+
+
     
-    if not valid:
-        print('Webcam could not be opened')
-        break
-
-    cv.imshow('Live Webcam',image)
-
-    if cv.waitKey(1) == ord('s'):
-        while(True):
-
-
-
-    if cv.waitKey(1) == ord('q'):
-        print('Exiting')
-        break
-
-
 
 cap.release()
 cv.destroyAllWindows()
